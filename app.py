6cb1e1b7df21ed932e707f3608ed726e016020ff
Andriy <shyryak1111@gmail.com>
2025-01-06 22:43:58 +0200

    worked in form

--- app.py
+++ app.py
@@ -1,10 +1,54 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,flash,redirect,url_for
+from db_scripts import DataBaseManager
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+
+db = DataBaseManager('todo.db')
+currentuser = 1
 
 app = Flask(__name__)
+app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route("/")  # Вказуємо url-адресу для виклику функції
 def index():
     return render_template("home.html")     
+
+
+
+
+@app.route("/current")
+def currenttodos():
+    todos = db.get_all_todos(currentuser)
+    return render_template("currenttodos.html",todos = todos)
+    
+@app.route("/todo/new",methods=["GET","POST"])
+def new_article():
+    if request.method == 'POST':
+        db.add_article(request.form['title'],request.form['text'],1,request.form['important'])
+        flash('Завдання додано','alert-success')
+
+    return render_template('newtodo.html')
+
+
+@app.route('/articles/<int:id>')
+def todo(id):
+    todo = db.get_article(id)
+    return render_template("viewtodo.html",todo=todo)
+@app.route('/articles/<int:id>')
+def todo():
+    if request.method == 'POST':
+        todo = db.save_article(request.form['title'],request.form['text'])
+        todo()
+    return redirect(url_for('currenttodos'))
+@app.route('/articles/<int:id>')
+def deletetodo(id):
+    if request.method == 'POST':
+        db.delete_todo(request.form['title'],request.form['text'])
+    return redirect(url_for('currenttodos'))
+
+
 
 
 if __name__ == "__main__":
---
+++ db_scripts.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+class DataBaseManager:
+    def __init__(self,dbname):
+        self.conn = None
+        self.cursor = None
+        self.dbname=dbname
+    def open(self):
+        self.conn = sqlite3.connect(self.dbname)
+        self.cursor = self.conn.cursor()
+    def close(self):
+        self.cursor.close()
+        self.conn.close()
+    
+    def get_all_todos(self,user_id):
+        self.open()
+        self.cursor.execute('''SELECT * FROM Todo WHERE user=?''',[user_id])
+        data = self.cursor.fetchall()
+        self.close()
+        return data
+    def add_article(self,title,memo,user,important):
+        self.open()
+        self.cursor.execute('''INSERT INTO Todo (title,memo,user,important) VALUES(?,?,?,?)''',[title,memo,user,int(important)])
+        self.conn.commit()
+        self.close()
+    def save_article(self,title,memo):
+        self.open()
+        self.cursor.execute('''INSERT INTO Todo (title,memo) VALUES(?,?)''',[title,memo])
+        self.conn.commit()
+        self.close()
+
+    def get_article(self,id):
+        self.open()
+        self.cursor.execute('''SELECT * FROM Todo WHERE id=? ''' ,[id])
+        data = self.cursor.fetchone()
+        self.close()
+        return data
+    
+    def delete_todo(self,id):
+        self.open()
+        self.cursor.execute('''DELETE FROM Todo WHERE id=? ''' ,[id])
+        data = self.cursor.fetchone()
+        self.close()
